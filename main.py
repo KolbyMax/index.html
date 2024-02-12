04d5f80c1cc87c5ffc354ad573813281d65e37f0
KolbyMax <158774044+KolbyMax@users.noreply.github.com>
2024-02-12 16:00:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def get_vowels(String):
     return [each for each in String if each in "aeiou"]
-get_vowels("animal") # [a, i, a]
+get_vowels("animal") # [r, g, b]
 get_vowels("sky") # []
 get_vowels("football") # [o, o, a]
 print(get_vowels)
